0e74828deba00a67701abd0b0f762966c12e3c3d
brian <liar88828@gmail.com>
2023-06-24 21:12:32 +0700

    12 Inheritance

---
+++ src/main/kotlin/app/inheritance.kt
@@ -0,0 +1,12 @@
+package app.app
+
+import app.data.Manager
+import app.data.VicePresident
+
+fun main() {
+	val manager = Manager("Eko")
+	manager.sayHello("Budi")
+
+	val vicePresident = VicePresident("Budi")
+	vicePresident.sayHello("Joko")
+}
