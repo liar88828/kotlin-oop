54faaa9560bb1f2e772781fa78256c0669677381
brian <liar88828@gmail.com>
2023-06-24 20:12:27 +0700

    6 Initializer Block

--- src/main/kotlin/data/Car.kt
+++ src/main/kotlin/data/Car.kt
@@ -6,4 +6,9 @@ class Car(
 ) {
 	var brand: String = paramBrand
 	var year: Int = paramYear
+
+	// secara otomatis akan di jalankan meskipun tidak di panggil method nya
+	init {
+		println("$paramBrand telah di buat")
+	}
 }
