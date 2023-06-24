b6212f8ea7f28af18a5a7350c561a6e63f9223f9
brian <liar88828@gmail.com>
2023-06-24 22:24:57 +0700

    19 toString Function

---
+++ src/main/kotlin/app/toString.kt
@@ -0,0 +1,10 @@
+package app.app
+
+import app.data.User
+
+fun main() {
+	val user = User("Eko", "Rahasia")
+	val user2 = User("Budi", "Rahasia")
+	println(user)
+	println(user2)
+}
--- src/main/kotlin/data/User.kt
+++ src/main/kotlin/data/User.kt
@@ -4,6 +4,10 @@ class User(
 	var username: String,
 	val password: String
 ) {
+//	langsung tanpa di panggil
+override fun toString(): String {
+	return "User with username = $username"
+}
 //	var username: String = usernameParam
 //	var password: String = passwordParam
 }
