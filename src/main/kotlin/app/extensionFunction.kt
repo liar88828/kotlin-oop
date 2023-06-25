4e20aeb0f58f0100114c19c2b4d6d2ebb474e8d5
brian <liar88828@gmail.com>
2023-06-25 11:12:08 +0700

    29 Extension Property

--- src/main/kotlin/app/extensionFunction.kt
+++ src/main/kotlin/app/extensionFunction.kt
@@ -2,9 +2,11 @@ package app.app
 
 import app.data.Student
 import app.data.sayHello
+import app.data.upperName
 
 fun main() {
-	val student = Student("Eko", 30)
-	println(student.age)
+	val student: Student? = Student("Eko", 30)
+	println(student?.age)
 	student.sayHello("Budi")
+	println(student?.upperName)
 }
