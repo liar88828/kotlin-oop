6adbc34ff5374ea3bf846d531d7496ade89cae5f
brian <liar88828@gmail.com>
2023-06-25 13:21:49 +0700

    41 Observable Property

---
+++ src/main/kotlin/app/ObserverProperty.kt
@@ -0,0 +1,11 @@
+package app.app
+
+import app.data.Account
+
+fun main() {
+	val account = Account()
+	account.descripton = "Contoh"
+	account.descripton = "di Ubah"
+	account.descripton = "hello world"
+
+}
--- src/main/kotlin/data/Account.kt
+++ src/main/kotlin/data/Account.kt
@@ -1,9 +1,18 @@
 package app.data
 
-class Account {
+import kotlin.properties.Delegates
+
+class Account(description: String = "") {
 	val name: String by lazy {
 		println("lazy name is called")
 		"Eko"
 	}
 //	val name: String = "Eko"
+
+
+	var descripton: String by Delegates.observable(description)
+	{ property, oldValue, newValue ->
+		println("${property.name} is change from $oldValue and $newValue ")
+	}
+
 }
